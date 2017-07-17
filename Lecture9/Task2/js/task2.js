91f6787db6178359cec64402003170e0da22f090
Liliya Tserkovna <lilichkatserkovna@gmail.com>
2017-07-17 22:19:32 +0300

    add some changes

--- Lecture9/Task2/js/task2.js
+++ Lecture9/Task2/js/task2.js
@@ -1,4 +1,4 @@
-let answer = true;
+let answer = confirm('Чи бажаєте почати гру?');
 while(answer === true) {
 	let gamerMoney = 0,
 		max = 5,
@@ -9,89 +9,84 @@ while(answer === true) {
 		firstAttempMoney = 10,
 		secondAttempMoney = 5,
 		thirdAttempMoney = 2;
-	answer = confirm('Чи бажаєте почати гру?');
-	if(answer === true) {
-			randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-			console.log(randomNumber);
-			attemps = 3;
-			while(attemps > 0) {
-				gamerNumber = parseInt(prompt(`Введіть будь-яке число від ${min} до ${max}:`, 0));
-				if ((gamerNumber >= min) && (gamerNumber <= max)){
-					if(randomNumber === gamerNumber) {
-						switch(attemps) {
-							case 3:
-								gamerMoney = firstAttempMoney;
-								break;
-							case 2:
-								gamerMoney = secondAttempMoney;
-								break;
-							case 1:
-								gamerMoney = thirdAttempMoney;
-								break;
-						}
-						alert(`Ви вгадали! Поточний виграш: ${gamerMoney}$`);
-						answer = confirm('Чи бажаєте продовжити гру?');
-						while (answer === true) {
-							attemps = 3;
-							max *= 2;
-							firstAttempMoney *= 3;
-							secondAttempMoney *= 3;
-							thirdAttempMoney *= 3;
-							randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-							console.log(randomNumber);
-							while(attemps > 0) {
-								gamerNumber = parseInt(prompt(`Введіть будь-яке число від ${min} до ${max}:`, 0));
-								if ((gamerNumber >= min) && (gamerNumber <= max)){
-									if(randomNumber === gamerNumber) {
-										switch(attemps) {
-											case 3:
-												gamerMoney += firstAttempMoney;
-												break;
-											case 2:
-												gamerMoney += secondAttempMoney;
-												break;
-											case 1:
-												gamerMoney += thirdAttempMoney;
-												break;
-										}
-										alert(`Ви вгадали! Поточний виграш: ${gamerMoney}$`);
-										answer = confirm('Чи бажаєте продовжити гру?');
+		randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+	console.log(`Секретне число = ${randomNumber}`);
+	while(attemps > 0) {
+		gamerNumber = parseInt(prompt(`Введіть будь-яке число від ${min} до ${max}:`, 0));
+		if ((gamerNumber >= min) && (gamerNumber <= max)){
+			if(randomNumber === gamerNumber) {
+				switch(attemps) {
+					case 3:
+						gamerMoney = firstAttempMoney;
+						break;
+					case 2:
+						gamerMoney = secondAttempMoney;
+						break;
+					case 1:
+						gamerMoney = thirdAttempMoney;
+						break;
+				}
+				alert(`Ви вгадали! Поточний виграш: ${gamerMoney}$`);
+				answer = confirm('Чи бажаєте продовжити гру?');
+				while (answer === true) {
+					attemps = 3;
+					max *= 2;
+					firstAttempMoney *= 3;
+					secondAttempMoney *= 3;
+					thirdAttempMoney *= 3;
+					randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+					console.log(`Секретне число = ${randomNumber}`);
+					while(attemps > 0) {
+						gamerNumber = parseInt(prompt(`Введіть будь-яке число від ${min} до ${max}:`, 0));
+						if ((gamerNumber >= min) && (gamerNumber <= max)){
+							if(randomNumber === gamerNumber) {
+								switch(attemps) {
+									case 3:
+										gamerMoney += firstAttempMoney;
 										break;
-									}
-									else {
-									--attemps;
-									alert(`Ви не вгадали! Залишилось спроб: ${attemps}!`);
-									}
+									case 2:
+										gamerMoney += secondAttempMoney;
+										break;
+									case 1:
+										gamerMoney += thirdAttempMoney;
+										break;
 								}
-								else {
-									alert("Введено некоректне число!");
-								}
-							}
-							if(attemps === 0) {
-								gamerMoney = 0;
+								alert(`Ви вгадали! Поточний виграш: ${gamerMoney}$`);
+								answer = confirm('Чи бажаєте продовжити гру?');
 								break;
 							}
+							else {
+								--attemps;
+								alert(`Ви не вгадали! Залишилось спроб: ${attemps}!`);
+							}
 						}
-						
+						else {
+							alert("Введено некоректне число!");
+						}
 					}
-					else {
-						--attemps;
-						alert(`Ви не вгадали! Залишилось спроб: ${attemps}!`)
+					if(attemps === 0) {
+						gamerMoney = 0;
+						break;
 					}
-				}
-				else {
-					alert("Введено некоректне число!");
-				}
-				if(answer === false) {
-					break;
-				}
+				}		
 			}
-			if(gamerMoney === 0) {
-				alert(`Ваш виграш - ${gamerMoney}`);	
+			else {
+				--attemps;
+				alert(`Ви не вгадали! Залишилось спроб: ${attemps}!`)
 			}
+		}
+		else {
+			alert("Введено некоректне число!");
+		}
+		if(answer === false) {
+			break;
+		}
+	}
+	if(gamerMoney === 0) {
+		alert(`Ваш виграш - ${gamerMoney}`);	
 	}
 	if((answer === false) || (gamerMoney === 0)) {
 		alert('Сьогодні ви не виграли мільйон, а могли!');
-		answer = true;
+		answer = confirm('Чи бажаєте почати гру?');
 	}
 } 
