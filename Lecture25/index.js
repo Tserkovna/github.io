eb4b68a5d0fa739a23792f5356bad12f1b4bc418
Tserkovna <lilichkatserkovna@gmail.com>
2017-09-19 00:40:42 +0300

    Update index.js

--- Lecture25/index.js
+++ Lecture25/index.js
@@ -36,7 +36,7 @@ presidentsCollection.push( new President( { id: 5, name: 'Petro', surname: 'Poro
 
 var PresidentView = Backbone.View.extend( {
     tagName: 'tr',
-    template: _.template('<td><%= id %>.<td><%= name %></td> <td><%= surname %></td> <td><%=rate %></td> <td><button class="delete-button">Delete</button></td>'),
+    template: _.template('<td><%= id %>.<td><%= name %></td> <td><%= surname %></td> <td><%=rate %></td> <td><button class="edit-button">Edit</button><button class="delete-button">Delete</button></td>'),
     render: function(){
         this.$el.html( this.template( this.model.toJSON() ) );
         return this; 
@@ -67,18 +67,54 @@ var DeleteButtonView = Backbone.View.extend( {
             for( let i=0; i<presidentsCollection.models.length; i++ ) {
                 if( id == presidentsCollection.models[i].attributes.id ) {
                    presidentsCollection.models.splice(i,1);
-                    break;
+                   break;
                 }
             }
             $('tbody').empty();
             presidents.render();
             remove.initialize();
+            inputs.initialize();
         });
     }
 });
 
+var InputsView = Backbone.View.extend({
+    initialize: function () {
+        let id;
+        $('.edit-button').on('click', function () {
+            id = event.target.closest('tr').childNodes[0].innerText.split('.', 1)[0];
+            let obj = {};
+            for( let i=0; i<presidentsCollection.models.length; i++ ) {
+                if( id == presidentsCollection.models[i].attributes.id ) {
+                   obj = presidentsCollection.models[i].attributes;
+                   break;
+                }
+            }
+            $('.form-name').val(obj.name);
+            $('.form-surname').val(obj.surname);
+            $('.form-rate').val(obj.rate);
+            $('.edit-form' ).show();
+        });
+        $('.change-button').on('click', function () {
+            for( let i=0; i<presidentsCollection.models.length; i++ ) {
+                if( id == presidentsCollection.models[i].attributes.id ) {
+                    presidentsCollection.models[i].attributes.name =  $('.form-name').val();
+                    presidentsCollection.models[i].attributes.surname =  $('.form-surname').val();
+                    presidentsCollection.models[i].attributes.rate =  $('.form-rate').val();
+                    break;
+                }
+            }
+            $('.edit-form' ).hide();
+            $('tbody').empty();
+            presidents.render();
+            remove.initialize();
+            inputs.initialize();
+        });
+    }
+});
 
+$('.edit-form' ).hide();
 var presidents = new PresidentsListView( { model: presidentsCollection } );
 $( '.table' ).append( presidents.render().$el );
 var remove = new DeleteButtonView(presidents);
- 
+var inputs = new InputsView(presidents);
